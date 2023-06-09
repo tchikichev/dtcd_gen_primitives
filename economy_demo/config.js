27110acc9abf1246b39636f6136b4d608b5a946c
Timur Chikichev <tchikichev@isgneuro.com>
2023-06-09 15:28:16 +0300

    raw primitives upload

---
+++ economy_demo/config.js
@@ -0,0 +1,23 @@
+let customActionTitle = "Solver"
+console.log(`Custom action "${customActionTitle}" started`)
+let workspaceSystem = Application.autocomplete.WorkspaceSystem
+
+let liveDashPanelUDNG = Application.autocomplete.LiveDashPanel_6;
+let liveDashPanelUtoRoads = Application.autocomplete.LiveDashPanel_7;
+let liveDashPanelUtoWaterAvia = Application.autocomplete.LiveDashPanel_8;
+let liveDashPanelUtoAT = Application.autocomplete.LiveDashPanel_9;
+let liveDashPanelUtoEE = Application.autocomplete.LiveDashPanel_10;
+
+// let liveDashPanel = Application.autocomplete.LiveDashPanel_2
+// let liveDashPanelWhatIf = Application.autocomplete.LiveDashPanel_3
+
+let dataSourceSystem = Application.autocomplete.DataSourceSystem
+let notificationSystem = Application.autocomplete.NotificationSystem
+let notificationSettingsSuccess = {type: "success", floatTime: 5, floatMode: true}
+let notificationSettingsFail = {type: "fail", floatTime: 5, floatMode: true}
+let notificationSettingsInfo = {type: "info", floatTime: 5, floatMode: true}
+
+// notificationSystem.create(customActionTitle, "Ожидайте загрузки страницы", notificationSettingsInfo)
+
+
+
